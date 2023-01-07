b52eb7d6dd88a600da9801b15fe33c8b79196d75
POTATOX <81747015+POTATOX35@users.noreply.github.com>
2023-01-07 12:12:41 +0000

    main.py

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ RPC = Presence(client_id)
 RPC.connect()
 
 while True:
+    if ("ShareX.exe" in (i.name() for i in psutil.process_iter()))== True:
+        RPC.update(
+            state= "ShareX Oynuyor",
+        )
     issic= round(psutil.cpu_percent(),1)
-   
     isram= round(psutil.virtual_memory().percent,1)
     RPC.update(
         details="CPU Usage= "+ str(issic)+"%",
-        state= str("chrome.exe" in (i.name() for i in psutil.process_iter())),
+        
         buttons= [{"label": "GitHub", "url": "https://github.com/POTATOX35/pcstats-discrd-richpresence"}],
          small_text="zort",
         start=int(time.time())

4459a00641cc6cf8f0526fcac5e152934df2a8fc
Yiğit Kaya <75912638+yigitkaya24@users.noreply.github.com>
2023-01-07 20:53:34 +0000

    allahım

--- main.py
+++ main.py
@@ -15,13 +15,19 @@ while True:
     window=findwhat(windows, " - Inkscape")
     if  len(window) >=  1:
          project_name=window[0]
-
+    mayso = project_name
+    if project_name == "":
+        mayso = "Idling..."
+    else:
+        mayso = "Editing " + project_name
+        
     if ("inkscape.exe" in (i.name() for i in psutil.process_iter())) == True:
         if (lastname != project_name):
+
             RPC.connect()
             lastname = project_name
             RPC.update(
-            state="Editing " + project_name,
+            state=mayso
             large_image="logo1",
             large_text="Playing Inkscape",
             start=int(time.time())

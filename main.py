97609b467e0980798c3a9cb14324d631fc53ca5e
POTATOX <81747015+POTATOX35@users.noreply.github.com>
2023-01-07 12:22:07 +0000

    main.py

--- main.py
+++ main.py
@@ -7,17 +7,22 @@ client_id ="842103064449253413"
 RPC = Presence(client_id)
 RPC.connect()
 
-while ("ShareX.exe" in (i.name() for i in psutil.process_iter()))== True:
-    
-    issic= round(psutil.cpu_percent(),1)
+while True:
+    if ("ShareX.exe" in (i.name() for i in psutil.process_iter())) == True:
+        issic= round(psutil.cpu_percent(),1)
     isram= round(psutil.virtual_memory().percent,1)
     RPC.update(
         details="CPU Usage= "+ str(issic)+"%",
         
         buttons= [{"label": "GitHub", "url": "https://github.com/POTATOX35/pcstats-discrd-richpresence"}],
-         small_text="zort",
+        small_text="zort",
         start=int(time.time())
     )
     time.sleep(5)
-    
+    if ("ShareX.exe" in (i.name() for i in psutil.process_iter())) == False:
+          issic= round(psutil.cpu_percent(),1)
+    isram= round(psutil.virtual_memory().percent,1)
+    RPC.update(
+        details=" "
+    )
     

65ec8d7e8ccaeae3ee24339266cb0c0f511fbaed
POTATOX <81747015+POTATOX35@users.noreply.github.com>
2023-01-07 09:01:10 +0000

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import psutil
+from pypresence import Presence
+import time
 
+
+
+client_id ="842103064449253413"
+RPC = Presence(client_id)
+RPC.connect()
+
+while True:
+    issic= round(psutil.cpu_percent(),1)
+   
+    isram= round(psutil.virtual_memory().percent,1)
+    RPC.update(details="CPU Usage= "+ str(issic)+"%" + "RAM Usage= "+str(isram)+"%")
+    time.sleep(5)

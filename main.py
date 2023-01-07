966c114a5aa51225f4b42e00fdf94049d6ab4518
POTATOX <81747015+POTATOX35@users.noreply.github.com>
2023-01-07 11:40:10 +0000

    main.py

--- main.py
+++ main.py
@@ -10,5 +10,9 @@ while True:
     issic= round(psutil.cpu_percent(),1)
    
     isram= round(psutil.virtual_memory().percent,1)
-    RPC.update(details="CPU Usage= "+ str(issic)+"%" ,state= "RAM Usage= "+str(isram)+"%")
+    RPC.update(
+        details="CPU Usage= "+ str(issic)+"%",
+        state= "RAM Usage= "+str(isram)+"%",
+        buttons= [{"label": "GitHub", "url": "https://github.com/POTATOX35/pcstats-discrd-richpresence"}]
+    )
     time.sleep(5)
